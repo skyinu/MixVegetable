8f22de556d80a8e4c49a72f8d3924f3dcc5025a0
skyinu <haochenu@gmail.com>
2019-12-04 23:20:06 +0800

    try print log & fix bug about library name

--- app/src/main/java/com/skyinu/jvmti/wrapper/MainActivity.kt
+++ app/src/main/java/com/skyinu/jvmti/wrapper/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        init {
+            System.loadLibrary("jvmti_wrapper")
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- libwrapper/src/main/cpp/jvmti-wrapper.c
+++ libwrapper/src/main/cpp/jvmti-wrapper.c
@@ -2,4 +2,12 @@
 // Created by skyinu on 2019/12/4.
 //
 
+#include <jni.h>
 #include"string.h"
+#include "android/log.h"
+
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    __android_log_print(ANDROID_LOG_INFO, "wrapper", "on library load");
+    return JNI_VERSION_1_6;
+}
+
