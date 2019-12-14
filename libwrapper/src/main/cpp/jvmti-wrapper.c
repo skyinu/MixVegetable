b4fc97b0606a25ad8093388b6a3a23506691fa9d
windy <haochenu@gmail.com>
2019-12-14 17:21:01 +0800

    config event and callback

--- libwrapper/src/main/cpp/jvmti-wrapper.c
+++ libwrapper/src/main/cpp/jvmti-wrapper.c
@@ -5,12 +5,13 @@
 #include <jni.h>
 #include"string.h"
 #include "applog/app_log.h"
-#include "jvmti.h"
+#include "jvmti-wrapper.h"
 #include "malloc.h"
 
 #define LOG_TAG "TAG_WRAPPER"
 #define MAX_LOG_LENGTH 500
 #define GENERAL_LOG_LENGTH 200
+
 
 jvmtiEnv *createJvmTiEnv(JavaVM *vm, jint version) {
     jvmtiEnv *jvmti;
@@ -35,6 +36,32 @@ jboolean addAndCheckCapabilities(jvmtiEnv *jvmti) {
     }
 }
 
+void configEvent(jvmtiEnv *jvmti) {
+    jint start = JVMTI_MIN_EVENT_TYPE_VAL;
+    jint end = JVMTI_MAX_EVENT_TYPE_VAL;
+    for (int index = start + 1; index < end; ++index) {
+        jvmtiError error = (*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE,
+                                                              (jvmtiEvent) index, NULL);
+        if (error != NULL) {
+            char log[GENERAL_LOG_LENGTH];
+            sprintf(log, "config event %d error, error code is %d", index, error);
+            loge(LOG_TAG, log);
+        }
+    }
+}
+
+void configCallback(jvmtiEnv *jvmti) {
+    jvmtiEventCallbacks callbacks;
+    (void) memset(&callbacks, 0, sizeof(callbacks));
+    callbacks.MethodEntry = &wrapperMethodEntry;
+    jvmtiError error = (*jvmti)->SetEventCallbacks(jvmti, &callbacks, (jint) sizeof(callbacks));
+    if (error != NULL) {
+        char log[GENERAL_LOG_LENGTH];
+        sprintf(log, "config callback  error, error code is %d", error);
+        loge(LOG_TAG, log);
+    }
+}
+
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     logi(LOG_TAG, "on library load");
     return JNI_VERSION_1_6;
@@ -48,6 +75,17 @@ JNIEXPORT jint JNICALL Agent_OnAttach(JavaVM *vm, char *options, void *reserved)
     free(log);
     jvmtiEnv *jvmti = createJvmTiEnv(vm, JVMTI_VERSION_1_2);
     addAndCheckCapabilities(jvmti);
+    configEvent(jvmti);
+    configCallback(jvmti);
     return JNI_OK;
 }
+
+//callback area start
+void wrapperMethodEntry(jvmtiEnv *jvmti_env,
+                        JNIEnv *jni_env,
+                        jthread thread,
+                        jmethodID method) {
+    logi(LOG_TAG, "wrapperMethodEntry");
+}
+//callback area end
 
